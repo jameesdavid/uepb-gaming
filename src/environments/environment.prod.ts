d621c8822121a58bfae6bba2e911701db6f00805
jameesdavid <jameesdavid@hotmail.com.br>
2022-08-26 18:19:27 -0300

    feat: setting up environments for deploy with github secrets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,13 @@
-import { firebaseEnv } from './firebase.environments';
-
 export const environment = {
   production: true,
-  firebase: firebaseEnv
+  firebase: {
+    projectId: '{UEPB_GAMING_PROJECT_ID}',
+    appId: '{UEPB_GAMING_APP_ID}',
+    storageBucket: '{UEPB_GAMING_STORAGE_BUCKET}',
+    locationId: '{UEPB_GAMING_LOCATION_ID}',
+    apiKey: '{UEPB_GAMING_API_KEY}',
+    authDomain: '{UEPB_GAMING_AUTH_DOMAIN}',
+    messagingSenderId: '{UEPB_GAMING_MESSAGING_SENDER_ID}',
+    measurementId: '{UEPB_GAMING_MEASUREMENT_ID}',
+  }
 };
